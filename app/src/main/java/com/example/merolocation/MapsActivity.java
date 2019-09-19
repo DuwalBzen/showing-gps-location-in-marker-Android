d665ae4cc05ea7eac2a263f1e968fc9484bb07db
DuwalBzen <bijen857@outlook.com>
2019-09-19 13:49:55 +0545

    Initial commot

---
+++ app/src/main/java/com/example/merolocation/MapsActivity.java
@@ -0,0 +1,144 @@
+package com.example.merolocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private Double myLatitude;
+    private Double myLongitude;
+    private final int locationRequestCode = 0;
+    private LocationManager mlocationManager;
+    private LocationListener mLocationListener;
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mlocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+
+                myLatitude = location.getLatitude();
+                myLongitude = location.getLongitude();
+                Log.d("myloc","Latitude "+myLatitude +" Longitude " +myLongitude );
+                mMap.clear();
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                LatLng myLoc = new LatLng(myLatitude, myLongitude);
+                mMap.addMarker(new MarkerOptions().position(myLoc).title("My current location right now! "+" Latitude " +myLongitude +
+                        " Longitude " +myLongitude));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLoc, 16));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                Toast.makeText(getApplicationContext(),"onStatusChanged",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                Toast.makeText(getApplicationContext(),"onProviderEnabled",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Toast.makeText(getApplicationContext(),"onProviderDisabled",Toast.LENGTH_SHORT).show();
+
+               call();
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, locationRequestCode);
+            } else {
+                mlocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+            }
+        }
+
+        }
+
+    private void call() {
+        if( !mlocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ) {
+            new AlertDialog.Builder(this)
+                    .setTitle("GPS not enable")  // GPS not found
+                    .setMessage("Please enable gps ") // Want to enable?
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                   call();
+                                }
+                            },5000);
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                mlocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+            }
+        }
+    }
+
+
+}
